a1a2736e5ad0a3d0d1f3c2a070f0cdaafeaa5fa7
wangshi <wangshi@eamil.com>
2024-03-18 17:33:38 +0800

    缓存击穿

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -3,22 +3,28 @@ package com.hmdp.service.impl;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
 import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
+import com.hmdp.utils.RedisData;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -33,20 +39,18 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
+    private ExecutorService executorService= Executors.newFixedThreadPool(10);
     @Override
     public Result queryShopBYId(Long id) {
 //        Shop shop = queryWithThrough(String.valueOf(id));
-        Shop shop = queryWithWutex(String.valueOf(id));
+//        Shop shop = queryWithWutex(String.valueOf(id));
+        Shop shop = queryLogic(id);
         if (shop==null){
             return Result.fail("商品不存在");
         }
-
         return Result.ok(shop);
 
-
-
     }
-
 
     public Shop queryWithWutex(String id){
         String key = CACHE_SHOP_KEY + id;
@@ -54,10 +58,11 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         if (!entries.isEmpty()) {
 
             Shop shop = BeanUtil.fillBeanWithMap(entries, new Shop(), false);
-            System.out.println(shop);
-            System.out.println(JSONUtil.toJsonStr(shop));
+//            System.out.println(shop);
+//            System.out.println(JSONUtil.toJsonStr(shop));
             return shop;
         }
+
         if(entries.get(id)!=null){
             log.debug("111");
             log.debug(entries.toString());
@@ -72,7 +77,7 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
                 Thread.sleep(20);
             }
             Shop byId = getById(id);
-            Thread.sleep(200);
+//            Thread.sleep(200);
 //        System.out.println(JSONUtil.toJsonStr(byId));
             if(byId!=null) {
                 Map<String, Object> stringObjectMap = BeanUtil.beanToMap(byId,new HashMap<>(),
@@ -92,6 +97,7 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
 
                 stringRedisTemplate.opsForHash().putAll(key, stringObjectMap);
+//                stringRedisTemplate.expire(key,)
                 stringRedisTemplate.expire(key, CACHE_SHOP_TTL, TimeUnit.MINUTES);
                 return byId;
             }
@@ -109,6 +115,40 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     }
 
+    public Shop queryLogic(Long id)  {
+        String key = CACHE_SHOP_KEY + id;
+        // 1、从redis中查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        // 2、判断是否存在
+        if (StrUtil.isBlank(shopJson)) {
+            // 存在,直接返回
+            return null;
+        }
+        RedisData redisData = JSONUtil.toBean(shopJson, RedisData.class);
+        JSONObject data = (JSONObject) redisData.getData();
+        Shop shop = JSONUtil.toBean(data, Shop.class);
+        LocalDateTime expireTime = redisData.getExpireTime();
+        if(expireTime.isAfter(LocalDateTime.now())){
+            return shop;
+        }
+        String lock=LOCK_SHOP_KEY+id;
+        if (getLock(lock)) {
+            executorService.submit(()-> {
+                        try {
+                            this.saveShop(id,10L);
+                        } catch (Exception e) {
+                            throw new RuntimeException();
+                        } finally {
+                            delLock(lock);
+                        }
+                    }
+            );
+        }
+        return shop;
+
+
+
+    }
     public Shop queryWithThrough(String id){
         String key = CACHE_SHOP_KEY + id;
         Map<Object, Object> entries = stringRedisTemplate.opsForHash().entries(key);
@@ -124,7 +164,6 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             log.debug(entries.toString());
             return null;
         }
-
 
         Shop byId = getById(id);
 //        System.out.println(JSONUtil.toJsonStr(byId));
@@ -142,8 +181,6 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
                                 }
                                 return fieldValue;
                             }));
-
-
 
             stringRedisTemplate.opsForHash().putAll(key, stringObjectMap);
             stringRedisTemplate.expire(key, CACHE_SHOP_TTL, TimeUnit.MINUTES);
@@ -179,5 +216,20 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
         stringRedisTemplate.delete(CACHE_SHOP_KEY + id);
         return Result.ok();
+    }
+
+    public void saveShop(Long id,Long expire){
+        Shop byId = getById(id);
+        LocalDateTime localDateTime = LocalDateTime.now().plusSeconds(expire);
+        RedisData redisData = new RedisData();
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        redisData.setData(byId);
+        redisData.setExpireTime(localDateTime);
+//        stringRedisTemplate.opsForValue().set("2",redisData.toString());
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY+id,JSONUtil.toJsonStr(redisData));
     }
 }
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,12 +1,20 @@
 package com.hmdp;
 
+import com.hmdp.service.impl.ShopServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
+@Autowired
+private ShopServiceImpl shopService;
 
-
+    @Test
+    void saveShop() {
+        shopService.saveShop(1L,12L);
+    }
 
 
 }
